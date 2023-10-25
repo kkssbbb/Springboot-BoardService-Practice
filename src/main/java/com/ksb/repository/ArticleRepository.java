850272735ff01d8124bedd00c376be75e9f5c6db
KING_ <89888075+kkssbbb@users.noreply.github.com>
2023-10-25 22:25:40 +0900

    #1- Querydsl 이용하여 검색 기능에 옵션 기능 추
    `QuerydslPredicateExecutor`
    `QuerydslBinderCustomizer`
    - 까먹었으면 위에 클래스 문서 찾아보고 오버라이딩한 customize() 메서드 설정 문서에서 찾아봐라  승빈아

--- src/main/java/com/ksb/repository/ArticleRepository.java
+++ src/main/java/com/ksb/repository/ArticleRepository.java
@@ -1,9 +1,30 @@
 package com.ksb.repository;
 
 import com.ksb.domain.Article;
+import com.ksb.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+   default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true); //true = Aticle 의 필드에대헤서 선택적으로 검색 가능
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); //검색 필터로 원하는 필드 선택
+        bindings.bind(root.title).first((StringExpression::containsIgnoreCase));
+        bindings.bind(root.content).first((StringExpression::containsIgnoreCase));
+        bindings.bind(root.hashtag).first((StringExpression::containsIgnoreCase));
+        bindings.bind(root.createdAt).first((DateTimeExpression::eq));
+        bindings.bind(root.createdBy).first((StringExpression::eq));
+    }
 }
